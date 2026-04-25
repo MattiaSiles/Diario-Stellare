3d7e252825418b9bc537dd53d6e50c51f4bff49e
MattiaSiles <sileimattia@gmail.com>
2026-04-25 17:47:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'diario-stellare-v2';
+const CACHE_NAME = 'diario-stellare-v3';
 
 // Questa è la lista delle cose da "scaricare" per far funzionare l'app offline
 const ASSETS_TO_CACHE = [
